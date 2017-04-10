d1250508f592822653898f435d261a032971f182
Niko <nikosheng@users.noreply.github.com>
2017-04-10 20:29:57 +0800

    Update getcpuprocstat.c

--- getcpuprocstat.c
+++ getcpuprocstat.c
@@ -6,61 +6,89 @@
 #include <fcntl.h>
 #include <sys/types.h>
 
-#define BUFFSIZE (64*1024)
+#define BUFFSIZE (1024)
 typedef unsigned long long jiff;
 
-void getstat(jiff *__restrict cuse, jiff *__restrict cice, jiff *__restrict csys, jiff *__restrict cide, jiff *__restrict ciow, jiff *__restrict cxxx, jiff *__restrict cyyy, jiff *__restrict czzz);
+void getstat(char* core, 
+	         jiff *__restrict cuse, 
+	         jiff *__restrict cice, 
+	         jiff *__restrict csys, 
+	         jiff *__restrict cide, 
+	         jiff *__restrict ciow, 
+	         jiff *__restrict cxxx, 
+	         jiff *__restrict cyyy, 
+	         jiff *__restrict czzz);
 
 
-int main(){
+int main(int argc, char **argv){
+	jiff  cuse;
+    jiff  cice;
+    jiff  csys;
+    jiff  cide;
+    jiff  ciow;
+    jiff  cxxx;
+    jiff  cyyy;
+    jiff  czzz;
+    char *core = argv[1];
 
-    while(1){
-	    jiff  cuse;
-	    jiff  cice;
-	    jiff  csys;
-	    jiff  cide;
-	    jiff  ciow;
-	    jiff  cxxx;
-	    jiff  cyyy;
-	    jiff  czzz;
+    if(core == NULL)
+    	core = "0";
 
-	    getstat(&cuse,
-	                &cice,
-	                &csys,
-	                &cide,
-	                &ciow,
-	                &cxxx,
-	                &cyyy,
-	                &czzz);
-    }
+    getstat(core,
+		&cuse,
+        &cice,
+        &csys,
+        &cide,
+        &ciow,
+        &cxxx,
+        &cyyy,
+        &czzz);
 }
 
 
-void getstat(jiff *restrict cuse, jiff *restrict cice, jiff *restrict csys, jiff *restrict cide, jiff *restrict ciow, jiff *restrict cxxx, jiff *restrict cyyy, jiff *restrict czzz)
+void getstat(char* core, 
+			 jiff *restrict cuse, 
+			 jiff *restrict cice, 
+			 jiff *restrict csys, 
+			 jiff *restrict cide, 
+			 jiff *restrict ciow, 
+			 jiff *restrict cxxx, 
+			 jiff *restrict cyyy, 
+			 jiff *restrict czzz)
 {
-
     static int fd;
     static char buff[BUFFSIZE];
     buff[BUFFSIZE-1] = 0;
     char* b;
+    int core_num = 0;
+    char prefix[6] = "cpu";
+    jiff temp, diff;
 
-  	if(fd){
-    	lseek(fd, 0L, SEEK_SET);
-  	}else{
-    	fd = open("/proc/stat", O_RDONLY, 0);
-    	if(fd == -1) printf("/proc/stat");
-  	}
+    strcat(prefix, core);
 
-    read(fd,buff,BUFFSIZE-1);
-    b = strstr(buff, "cpu ");
-    *ciow = 0;  /* not separated out until the 2.5.41 kernel */
-  	*cxxx = 0;  /* not separated out until the 2.6.0-test4 kernel */
-  	*cyyy = 0;  /* not separated out until the 2.6.0-test4 kernel */
-  	*czzz = 0;  /* not separated out until the 2.6.11 kernel */
-    if(b) 
-    	sscanf(b,  "cpu  %Lu %Lu %Lu %Lu %Lu %Lu %Lu %Lu", cuse, cice, csys, cide, ciow, cxxx, cyyy, czzz);
+    while(1){
+	    if(fd){
+	    	lseek(fd, 0L, SEEK_SET);
+	  	}else{
+	    	fd = open("/proc/stat", O_RDONLY, 0);
+	    	if(fd == -1) printf("/proc/stat");
+	  	}
 
-    fprintf(stdout, "us: %Lu", *cuse);
-    usleep(1000 * 30);
+	    read(fd, buff, BUFFSIZE - 1);
+
+	    b = strstr(buff, prefix);
+	    *ciow = 0;  /* not separated out until the 2.5.41 kernel */
+	  	*cxxx = 0;  /* not separated out until the 2.6.0-test4 kernel */
+	  	*cyyy = 0;  /* not separated out until the 2.6.0-test4 kernel */
+	  	*czzz = 0;  /* not separated out until the 2.6.11 kernel */
+	    if(b){
+	    	sscanf(b,  "cpu%s  %Lu", core, cuse);
+	    	// sscanf(b,  "cpu  %Lu %Lu %Lu %Lu %Lu %Lu %Lu %Lu", cuse, cice, csys, cide, ciow, cxxx, cyyy, czzz);
+	    }
+	    	
+	    diff = *cuse - temp;
+	    fprintf(stdout, "%s - diff: %Lu us: %Lu\n", core, diff, *cuse);
+	    temp = *cuse;
+	    usleep(1000 * 10);
+    }
 }
-
